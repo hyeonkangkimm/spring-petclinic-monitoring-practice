ee8e6c27c56773cb7f3cadb591f7896dd9029619
hyeonkangkimm <kkhk75@gmail.com>
2026-02-22 19:37:52 +0900

    Feat: 레이턴시 추가(트러블 슈팅 목적)

---
+++ src/main/java/org/springframework/samples/petclinic/system/DebugPerfController.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.system;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/debug")
+public class DebugPerfController {
+
+	@GetMapping("/slow")
+	public String slow() throws InterruptedException {
+		Thread.sleep(2000); // 2초 지연
+		return "ok";
+	}
+}
